94a564ac4ff36907f181229568ce653cd1069e2c
Bell App Lab <apps@bellapplab.com>
2018-07-13 09:48:01 +0200

    Version 1.0

--- Headers/Laziable.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for Laziable_iOS.
-FOUNDATION_EXPORT double LaziableVersionNumber;
-
-//! Project version string for Laziable_iOS.
-FOUNDATION_EXPORT const unsigned char LaziableVersionString[];
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import Laziable
-
-XCTMain([
-    testCase(LazyTests.allTests)
-])
